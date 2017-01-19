a8731cec3e8d84c2241938347c8718c38f8b6601
David Garcia <dgarcia@ulcomm.com>
2017-01-19 16:05:57 -0600

    Added GA tages to main template.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -30,8 +30,27 @@
                 'csrfToken' => csrf_token(),
         ]); ?>
     </script>
+    
+    @if(App::environment('production'))
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MKJ4BZ');</script>
+    <!-- End Google Tag Manager -->
+    @endif
+    
 </head>
 <body id="background">
+	
+@if(App::environment('production'))
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MKJ4BZ"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+@endif
+	
 <div class="gray-bar"></div>
 <nav class="navbar navbar-default navbar-static-top">
     <div class="container">
